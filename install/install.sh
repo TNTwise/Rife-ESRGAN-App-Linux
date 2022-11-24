dc732ad001ad36a003dfe6f900512ce793897b47
Paxson Gottschalk <tntwise3@gmail.com>
2022-11-23 19:14:12 -0600

    modified:   install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,4 +4,4 @@ sudo apt install ffmpeg python3-opencv python3-tk tk python3-pip xterm
 sudo pacman -S tk opencv xterm ffmpeg python-pip
 cp Rife-Vulkan-GUI.desktop /home/$USER/.local/share/applications/
 mkdir /home/$USER/Rife-Vulkan-GUI
-cp -r * /home/$USER/Rife-Vulkan-GUI
+cp -r ../* /home/$USER/Rife-Vulkan-GUI
