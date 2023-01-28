8dba8b98dedec54df0d0570f2416274f4c4729b8
TNTwise <77559100+TNTwise@users.noreply.github.com>
2023-01-27 21:48:50 -0600

    Delete install directory

--- install/install.sh
+++
@@ -1,7 +0,0 @@
-sudo cp rife-gui /usr/bin/rife-gui
-sudo cp ../icons/Icon.svg /usr/share/icons/hicolor/scalable/apps/Rife.svg
-sudo apt install ffmpeg python3-opencv python3-tk tk python3-pip xterm 
-sudo pacman -S tk opencv xterm ffmpeg python-pip
-cp Rife-Vulkan-GUI.desktop /home/$USER/.local/share/applications/
-mkdir /home/$USER/Rife-Vulkan-GUI
-cp -r ../* /home/$USER/Rife-Vulkan-GUI
